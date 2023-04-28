c33d5bc00a35b78f027d772019a6503b31a67d06
SmarterTuition <131989779+SmarterTuition@users.noreply.github.com>
2023-04-28 13:47:01 +0200

    Update FileChooser.py

--- FileChooser.py
+++ FileChooser.py
@@ -1,3 +1,4 @@
+import os
 import numpy
 from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QFileDialog, QTextEdit
 from PyQt6 import uic
@@ -15,7 +16,6 @@ class FileChooser(QWidget):
         self.filename = None
         self.type = None
         self.loadedfilename = None
-        self.newfilename = None
         self.data = None
 
         uic.loadUi("ST_GUI2.ui", self)
@@ -37,10 +37,10 @@ class FileChooser(QWidget):
         if self.filename == "":
             return
         if self.filename.endswith(".xls") or self.filename.endswith(".xlsx"):
-            self.loadedfilename = self.filename.rsplit('/')
-            self.newfilename = "Excel Datei {fn} erfolgreich geladen!".format(fn='"' + self.loadedfilename[-1] + '"')
-            self.DataLabel.setText(self.newfilename)
-            self.data = pd.read_excel('{file}'.format(file=self.filename))
+            newfilename = f'Excel Datei "{os.path.basename(self.filename)}" erfolgreich geladen!'
+            self.DataLabel.setText(newfilename)
+            self.data = pd.read_excel(self.filename)
+            self.data = self.data.drop(['ID', 'Startzeit', 'Fertigstellungszeit', 'E-Mail', 'Name'], axis=1, errors='ignore')
 
     def auswertung(self):
 
@@ -53,6 +53,7 @@ class FileChooser(QWidget):
                 "Die ausgewählt Datei entspricht nicht dem vorgegebenem Format (15 Spalten)\nBitte überprüfen Sie die ausgewählte Datei")
         else:
             pd.set_option('display.expand_frame_repr', False)
+
             # Die Office-ID der Lehrkraft wird in unserem Fall nicht benötigt, da nur eine Lehrkraft die Umfragen
             # durchgeführt hat. Da diese aber in Zukunft von Nutzen sein könnte, wird diese in der Umfrage verbleiben.
 
@@ -61,51 +62,52 @@ class FileChooser(QWidget):
             # data = temp
 
             # Spalte 2 --- (Beurteilen Sie Ihre eigene Motivation in dem unterrichteten Fach)
-            data = self.data.iloc[0:self.data.shape[0], 1]
             # Spalte 3 --- (Den Unterricht meiner Lehrkraft empfinde ich als wertschätzend)
-            temp = self.data.iloc[0:self.data.shape[0], 2]
-            data = pd.concat([data, temp], axis=1)
             # Spalte 4 --- (Das Klassenklima empfinde ich als positiv)
-            temp = self.data.iloc[0:self.data.shape[0], 3]
-            data = pd.concat([data, temp], axis=1)
             # Spalte 5 --- (Ein sehr guter Ausbildungsabschluss ist für mich sehr wichtig)
-            temp = self.data.iloc[0:self.data.shape[0], 4]
-            data = pd.concat([data, temp], axis=1)
+            data = self.data.iloc[:, 1:5]
             # Spalte 6 OHE --- (Geschlecht)
-            temp1 = self.data.iloc[0:self.data.shape[0], 5]
-            temp = pd.get_dummies(temp1, drop_first=True)
+            temp = pd.get_dummies(pd.Categorical(self.data.iloc[:, 5], categories=["weiblich", "männlich"]),
+                                  dtype=int, drop_first=True)
             data = pd.concat([data, temp], axis=1)
             # Spalte 7 --- Alter
-            temp = self.data.iloc[0:self.data.shape[0], 6]
+            temp = self.data.iloc[:, 6]
             data = pd.concat([data, temp], axis=1)
             # Spalte 8 OHE --- (Aktuelle Ausbildungsrichtung)
-            temp1 = self.data.iloc[0:self.data.shape[0], 7]
-            temp = pd.get_dummies(temp1, drop_first=True)
+            temp = pd.get_dummies(pd.Categorical(self.data.iloc[:, 7], categories=["kaufmännisch",
+                                                                                   "gewerblich technisch"]),
+                                  dtype=int, drop_first=True)
             data = pd.concat([data, temp], axis=1)
             # Spalte 9 OHE --- Reguläre Ausbildungsdauer
-            temp1 = self.data.iloc[0:self.data.shape[0], 8]
-            temp = pd.get_dummies(temp1, drop_first=True)
+            temp = pd.get_dummies(pd.Categorical(self.data.iloc[:, 8], categories=["2 - 2,5-jährig",
+                                                                                   "mindestens 3-jährig"]),
+                                  dtype=int, drop_first=True)
             data = pd.concat([data, temp], axis=1)
             # Spalte 10 --- Aktuelle Jahrgangsstufe
-            temp = self.data.iloc[0:self.data.shape[0], 9]
+            temp = self.data.iloc[:, 9]
             data = pd.concat([data, temp], axis=1)
             # Spalte 11 OHE --- Ist Ihre Muttersprache Deutsch?
-            temp1 = self.data.iloc[0:self.data.shape[0], 10]
-            temp = pd.get_dummies(temp1, drop_first=True)
+            temp = pd.get_dummies(pd.Categorical(self.data.iloc[:, 10], categories=["Ja", "Nein"]),
+                                  dtype=int, drop_first=True)
             data = pd.concat([data, temp], axis=1)
             # Spalte 12 OHE --- Welche größe hat das Unternehmen in welchem Sie beschäftigt sind?
-            temp1 = self.data.iloc[0:self.data.shape[0], 11]
-            temp = pd.get_dummies(temp1, drop_first=True)
+            temp = pd.get_dummies(pd.Categorical(self.data.iloc[:, 11],
+                                                 categories=["Ich bin derzeit in keinem Unternehmen beschäftigt",
+                                                             "Kleines Unternehmen (bis 49 Personen)",
+                                                             "Mittleres Unternehmen (bis 249 Personen)",
+                                                             "Großunternehmen (über 249 Personen)"]),
+                                  dtype=int, drop_first=True)
             data = pd.concat([data, temp], axis=1)
             # Spalte 13 OHE --- Bisherige Schulbildung
-            temp1 = self.data.iloc[0:self.data.shape[0], 12]
-            temp = pd.get_dummies(temp1, drop_first=True)
+            temp = pd.get_dummies(pd.Categorical(self.data.iloc[:, 12], categories=["Mittelschulabschluss ohne Quali",
+                                                                                    "Mittelschulabschluss mit Quali",
+                                                                                    "Mittlere Reife Realschule",
+                                                                                    "Mittlere Reife Mittelschule",
+                                                                                    "Allgemeine Fachhochschulreife",
+                                                                                    "Allgemeine Hochschulreife (Abitur)",
+                                                                                    "Fachgebundene Fachhochschulreife"]),
+                                  dtype=int, drop_first=True)
             data = pd.concat([data, temp], axis=1)
-            # Spalte 14 --- Wurde Ihr Lernerfolg durch den Einsatz des digitalen Tools gefördert?
-            # temp = self.data.iloc[0:self.data.shape[0], 13]
-            # data = pd.concat([data, temp], axis=1)
-            # Spalte 15 als Label --- Würden Sie sich einen häufigeren Einsatz digitaler Tools im Unterricht wünschen?
-            # label = self.data.iloc[0:self.data.shape[0], 13]
 
             # XGBRegressor mit 150 Bäumen, einer max_depth von 5, learning_rate von 0.05 und early_stopping_rounds i.h.v. 15
             model = XGBRegressor()
